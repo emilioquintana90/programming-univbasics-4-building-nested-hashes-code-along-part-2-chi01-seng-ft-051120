768333f2572f74cf481f0650c9a3e0b3ab83b72a
Emilio Quintana <emilioquintana90@gmail.com>
2020-04-17 17:57:44 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -37,6 +37,7 @@ def add_characters
          :patriarch => {name: "Lord Montague", age: "53"},
          :matriarch => {name: "Lady Montague", age: "54"},
          :hero => {name: "Romeo", age: "15", status: "alive"},
+         :additional_characters => [{name: "Prince Escalus"},{name: "Apothecary"}],
          :hero_friends => [
             {name: "Benvolio", age: "17", attitude: "worried"},
             {name: "Mercutio", age: "18", attitude: "hot-headed"}
